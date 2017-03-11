55c5920d0df1beb612e36d306461fe6305519611
Tallis Hobbs <rhtallis@gmail.com>
2017-03-11 15:23:30 -0800

    event Service

--- angular/zenythangular/src/app/events/event-list/event-list.component.ts
+++ angular/zenythangular/src/app/events/event-list/event-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { EventService } from '../event.service';
+import { Event } from '../event';
 
 @Component({
   selector: 'app-event-list',
@@ -6,10 +8,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./event-list.component.css']
 })
 export class EventListComponent implements OnInit {
+  events: Event[];
+  listOfEvents: Object;
 
-  constructor() { }
+  constructor(private _eventService: EventService) { }
 
   ngOnInit() {
+    this._eventService.getAllEvents().subscribe(
+      // Success
+      data => {
+        console.log(data);
+        this.listOfEvents = data;
+      },
+      error => {
+        alert(error);
+      },
+      () => {
+        console.log("SUBSCRIPTION FINISHED: get all events")
+      }
+    );
   }
 
 }
--- angular/zenythangular/src/app/events/event.service.ts
+++ angular/zenythangular/src/app/events/event.service.ts
@@ -1,8 +1,53 @@
 import { Injectable } from '@angular/core';
+import { Event } from './event';
+import { Http, Response } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/observable/throw';
 
 @Injectable()
 export class EventService {
+  private eventsUrl = '/api/';
 
-  constructor() { }
+  constructor(private _http: Http) { }
+
+/***********************************************
+ * 
+ * RESTFUL Service Functions
+ * 
+ ***********************************************/
+
+
+  /*
+  * Function: GET all events
+  * get("/api/allevents")
+  * Url will need updating
+  */
+  getAllEvents() {
+    let url = this.eventsUrl + 'allevents';
+
+    return this._http.get(url)
+      .map(this.extractData)
+      .catch(this.handleError);
+  }
+
+
+  /***********************************************
+  * handleError
+  * Extract Data back into Json
+  ***********************************************/
+
+    // Retreival of JSON from .NET is a success.
+    private extractData(res: Response) {
+        let body = res.json();
+        return body || {};
+    }
+
+  private handleError(error: any) {
+    let errMsg = (error.message) ? error.message :
+      error.status ? `${error.status} - ${error.statusText}` : 'Server error';
+    return Observable.throw(errMsg);
+  }
 
 }
