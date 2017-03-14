dfeaca7bd593db5f152ff8bda91c08664c548841
Tallis Hobbs <rhtallis@gmail.com>
2017-03-14 15:37:31 -0700

    Comments complete

--- angular/zenythangular/src/app/events/event-list/event-list.component.ts
+++ angular/zenythangular/src/app/events/event-list/event-list.component.ts
@@ -25,20 +25,8 @@ export class EventListComponent implements OnInit {
       // Success
       data => {
         this.events = data;
-
-        // this.events = data.map(event);
         console.log('Events:');
-        console.log(this.events);
-        // console.log(new Date().toLocaleString('en-us', { weekday: 'long', month: 'long' }));
-
-        // console.log(new Date().toLocaleString());
-        // console.log(this.events[0].activity_start.toLocaleString('en-us', { weekday: 'long'}));
-        console.log(new Date(data[0].activity_start).getDate());
-        // console.log(new Date(data[0].))
-
-
         this.setDisplayEvents(data)
-
       },
       error => {
         alert(error);
@@ -47,45 +35,52 @@ export class EventListComponent implements OnInit {
         console.log("SUBSCRIPTION FINISHED: get all events")
       }
     );
-
-    // this.startOfWeek = moment()
-
-
   }
 
   setDisplayEvents(thisWeeksEvents) {
+    const DATEOPTIONS = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+    const TIMEOPTIONS = { minute: 'numeric', hour: 'numeric' };
 
-    var dateOptions = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
-    var timeOptions = { minute: 'numeric', hour: 'numeric' };
-
+    /**
+     * insert first dateRow into evets_to_display Array
+     */
     let firstDay = new Displayevent();
-
     firstDay.activity_date = new Date(thisWeeksEvents[0].activity_start);
     firstDay.activity = "dateTableRow";
-    firstDay.activity_long_date = firstDay.activity_date.toLocaleDateString('en-US', dateOptions);
-    firstDay.activity_start = firstDay.activity_date.toLocaleTimeString('en-US', timeOptions);
-
+    firstDay.activity_long_date = firstDay.activity_date.toLocaleDateString('en-US', DATEOPTIONS);
+    firstDay.activity_start = firstDay.activity_date.toLocaleTimeString('en-US', TIMEOPTIONS);
     this.events_to_display.push(firstDay);
-    console.log(firstDay.activity_long_date);
-    console.log(firstDay.activity_start);
 
+    /**
+     * Iterate through api response -> generate formatted display events_to_display Array
+     * Dynamically insert header rows into events_to_display Array
+     */
     for (var index = 0; index < thisWeeksEvents.length; index++) {
 
       let thisActivity = new Displayevent();
       thisActivity.activity = thisWeeksEvents[index].activity;
       thisActivity.activity_date = new Date(thisWeeksEvents[index].activity_start);
-      thisActivity.activity_start = new Date(thisWeeksEvents[index].activity_start).toLocaleTimeString('en-US', timeOptions);
-      thisActivity.activity_end = new Date(thisWeeksEvents[index].activity_end).toLocaleTimeString('en-US', timeOptions);
-      thisActivity.activity_long_date = new Date(thisWeeksEvents[index].activity_start).toLocaleDateString('en-US', dateOptions);
+      thisActivity.activity_start = new Date(thisWeeksEvents[index].activity_start).toLocaleTimeString('en-US', TIMEOPTIONS);
+      thisActivity.activity_end = new Date(thisWeeksEvents[index].activity_end).toLocaleTimeString('en-US', TIMEOPTIONS);
+      thisActivity.activity_long_date = new Date(thisWeeksEvents[index].activity_start).toLocaleDateString('en-US', DATEOPTIONS);
       thisActivity.is_activity = true;
-      // this.events_to_display.push(thisActivity);
+
+      /**
+       * Compare thisWeekEvents[index] to last event added to events_to_display Array
+       * If thisWeekEvents[index] is on a new day, add dateTableRow to events_to_display Array
+       */
       if (thisActivity.activity_long_date != this.events_to_display[this.events_to_display.length - 1].activity_long_date) {
         let dateTableRow = new Displayevent();
-        // dateTableRow.activity = "dateTableRow";
+
         dateTableRow.is_activity = false;
         dateTableRow.activity_long_date = thisActivity.activity_long_date;
         this.events_to_display.push(dateTableRow);
       }
+
+      /**
+       * After confirmed dateTableRow is correct
+       * Insert thisWeeksEvents[index] to events_to_display Array
+       */
 
       /**
        * Check event is active 
@@ -94,33 +89,8 @@ export class EventListComponent implements OnInit {
       if (thisWeeksEvents[index].is_active) {
         this.events_to_display.push(thisActivity);
       }
-
     }
-
     console.log(this.events_to_display);
-    // dayOfWeek = daysOfTheWeek[ firstDay.activity_date.getDay()];
-    // dayOfMonth = firstDay.activity_date.getDate().toString();
-    // month = firstDay.activity_date.getMonth().toString();
-
-
-
-
-
-    // firstDay.activity_long_date = 
-
-
-    // firstDay.activity_day_of_week = daysOfTheWeek[parseInt(new Date(thisWeeksEvents[0].activity_start.getDay()).toString())];
-    // // firstDay.activity_long_date = 
-
-    // for (var index = 0; index < thisWeeksEvents.length; index++) {
-    //   let currentDay = parseInt(new Date(thisWeeksEvents[0].activity_start.getDay()).toString());
-
-    //   let thisEvent = new Displayevent();
-
-
-    //   thisEvent.activity_day_of_week
-    // }
-
   }
 
 }
